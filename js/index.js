fb9677e0592a72461a5dbcb71e9b9e5d56ecba32
Chukwunonso prosper <114858924+Prosperdev@users.noreply.github.com>
2024-03-08 21:20:41 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+async function getIp() {
+    let query = document.querySelector("input").value.trim();
+    if (query == "") {
+        //
+    } else {
+        let result = (document.getElementById("res").style.display = "block");
+        const url = "https://community-neutrino-ip-info.p.rapidapi.com/ip-info";
+        const options = {
+            method: "POST",
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+                "X-RapidAPI-Key": "4273ea270amshe9b959a46322d44p1945b1jsn885a1014a2eb",
+                "X-RapidAPI-Host": "community-neutrino-ip-info.p.rapidapi.com",
+            },
+            body: new URLSearchParams({
+                ip: query,
+                "reverse-lookup": "checked",
+            }),
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            let parse = JSON.parse(result);
+            for (var key in parse) {
+                document.getElementById('pars').innerHTML += key;
+                document.getElementById('respi').innerHTML += parse[key]
+            }
+        } catch (error) {
+            document.getElementById('error').innerHTML = error.message;
+        }
+    }
+}
